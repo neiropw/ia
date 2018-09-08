a2176f81348b067011d3f274521c42584e2ce840
Tomotada Saigusa <nirot1r@g-second.net>
2018-09-08 17:08:09 +0900

    初期設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,31 @@
 module.exports = {
-    "extends": "standard"
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2015
+  },
+  "rules": {
+    "no-console": "off",
+    "no-var": "error",
+    "prefer-arrow-callback": [ "error", { "allowNamedFunctions": true } ],
+    "arrow-parens": ["error", "as-needed"],
+    "dot-location": ["error", "property"],
+    "prefer-template": 2,
+    "object-shorthand": ["error", 2],
+    "no-param-reassign": ["error", { "props": true }],
+    "no-else-return": "error",
+    "indent": [
+        "error",
+        2
+    ],
+    // "linebreak-style": [
+    //     "error",
+    //     "windows"
+    // ],
+    "quotes": ["error", "single"],
+    "semi": ["error", "never"]
+  }
 };
