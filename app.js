cc80f8474a883e4164929beebeb4232adffef90a
Tomotada Saigusa <nirot1r@g-second.net>
2018-09-08 20:25:13 +0900

    #15 正規表現の調整

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ client.on('message', msg => {
 
   // console.log(msg.content)
 
-  const reg_mention = new RegExp(`<@${client.user.id}>\s.*`)
+  const reg_mention = new RegExp(`<@${client.user.id}>[\s　].*`)
 
   if(reg_mention.test(msg.content)) {
     mention(msg)
