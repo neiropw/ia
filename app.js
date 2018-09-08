33e2a639d89c75d12284d53884a5f93a9654ce2a
Tomotada Saigusa <nirot1r@g-second.net>
2018-09-08 17:35:05 +0900

    #1 かわいい/は？/草ｗ/草/なくなった/わかる/ﾇﾍﾞｽｺ/ 🖕

--- app.js
+++ app.js
@@ -8,9 +8,43 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if(msg.content === 'かわいい') {
+  if(msg.author.id === client.user.id) return;
+
+  console.log(msg.content)
+
+  if(/かわいい*$/.test(msg.content)) {
     console.log('app[message]: かわいい')
-    msg.channel.send('ありがと！')
+    msg.react('❤')
+    msg.channel.send(`<@!${msg.author.id}> ありがと！`)
+  }
+  else if(/は[?？]/.test(msg.content)) {
+    console.log('app[message]: は？')
+    msg.react('🤔')
+  }
+  else if(/(?:草|くさ)[wｗ]/.test(msg.content)) {
+    console.log('app[message]: 草ｗ')
+    msg.react('💢')
+    msg.channel.send(`<@!${msg.author.id}> 草に草を生やすな`)
+  }
+  else if(/草$/.test(msg.content)) {
+    console.log('app[message]: 草')
+    msg.react('🌿')
+  }
+  else if(/なくなった$/.test(msg.content)) {
+    console.log('app[message]: なくなった')
+    msg.react('😢')
+  }
+  else if(/わかる/.test(msg.content)) {
+    console.log('app[message]: わかる')
+    msg.react('👍')
+  }
+  else if(/(՞ةڼ◔)/.test(msg.content)) {
+    console.log('app[message]: (՞ةڼ◔)')
+    msg.channel.send('(՞ةڼ◔) ｲﾋ-wwwww')
+  }
+  else if(/🖕/.test(msg.content)) {
+    console.log('app[message]: 🖕')
+    msg.react('🖕')
   }
 })
 
